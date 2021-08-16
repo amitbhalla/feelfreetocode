6e3f96d4ca24e48e7f75be384ed9520a32c367e5
amitbhalla <amit9815@gmail.com>
2021-08-16 19:05:40 +0530

    Completed course models

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Course, Category, Tag
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ['slug', 'title', 'id']
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_editable = ['active']
+    list_display = ['title', 'discount', 'active', 'id']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['tag', 'course', 'id']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Tag, TagAdmin)
