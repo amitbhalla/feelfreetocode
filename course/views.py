060871a63590b2077526444ee5b3614f691b2c76
amitbhalla <amit9815@gmail.com>
2021-08-17 13:25:31 +0530

    Save code for nested serializer

--- course/views.py
+++ course/views.py
@@ -1,5 +1,8 @@
+from django.core.exceptions import ValidationError
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from core.permissions import IsAdminUserOrReadOnly
 from course.serializers import CategorySerializer, CourseSerializer, TagSerializer
@@ -23,6 +26,39 @@ class CourseViewSet(ModelViewSet):
     permission_classes = [IsAdminUserOrReadOnly]
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        course = request.data  # Get request data
+        category_id = course.get('category_id')  # get category_id
+
+        if category_id is None:  # if the user didn't pass category_id throw an error
+            error_message = {
+                'category_id': ['category_id is required.']
+            }
+            return Response(error_message)
+
+        try:  # check if passed category_id is valid
+            category = Category.objects.get(pk=category_id)
+
+        # DoesNotExist checks for error
+        # ValidationError if pk is in valid uuid format
+        except Category.DoesNotExist or ValidationError:  
+            error_message = {
+                'category_id': ['category_id is not valid.']
+            }
+            return Response(error_message)
+
+        context = {
+            'request': request
+        }
+        serializer = CourseSerializer(data=course, context=context)
+
+        if serializer.is_valid():
+            courseInstance = Course(**serializer.validated_data, category=category)
+            courseInstance.save()
+            return Response(CourseSerializer(courseInstance, context=context).data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CourseSlugDetailView(RetrieveUpdateDestroyAPIView):
