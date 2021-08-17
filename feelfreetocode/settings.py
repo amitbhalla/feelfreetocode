987f9518f45fbee5cae74eeb80d7832092d249fa
amitbhalla <amit9815@gmail.com>
2021-08-17 21:51:17 +0530

    Revert "Added Django filtering module"
    This reverts commit fdf07383ae95c83c12eb1087ee8dda0d646c6c9f.

--- feelfreetocode/settings.py
+++ feelfreetocode/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_filters',
     'course',
     'chapter',
     'coupon',
@@ -139,7 +138,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
-}
