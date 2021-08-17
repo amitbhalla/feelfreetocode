9448167595b9078bbd9b1166eab9169e403ae860
amitbhalla <amit9815@gmail.com>
2021-08-17 22:51:37 +0530

    Filters

--- feelfreetocode/settings.py
+++ feelfreetocode/settings.py
@@ -49,7 +49,12 @@ INSTALLED_APPS = [
     'order',
     'review',
     'core',
+    'django_filters',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -138,3 +143,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
