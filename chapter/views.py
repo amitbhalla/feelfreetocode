4dd5ca19244e67c811132bbc403ee6e46477ea64
amitbhalla <amit9815@gmail.com>
2021-08-18 20:35:26 +0530

    Some refactoring of code

--- chapter/views.py
+++ chapter/views.py
@@ -10,26 +10,11 @@ def chapter_types_view(request):
         for _id, description in chapter_choises_description:  # Cannot use id twice hence _id
             if id == _id:
                 return description
-
-    def ChangeToDict(chapter_type):
-        id, type = chapter_type
-        return {
-            'id': id,
-            'type': type,
-            'description': SearchDescription(id),
-        }
-    types = map(ChangeToDict, chapter_choices)
+    types = map(lambda e: dict(id=e[0], type=e[1], description=SearchDescription(e[0])), chapter_choices)
     return Response(types)
 
 
 @api_view(['GET'])
 def video_platform_view(request):
-
-    def ChangeToDict(video_platforms):
-        id, type = video_platforms
-        return {
-            'id': id,
-            'platform': type,
-        }
-    platform = map(ChangeToDict, video_platform_choises)
+    platform = map(lambda e: dict(id=e[0], platform=e[1]), video_platform_choises)
     return Response(platform)
