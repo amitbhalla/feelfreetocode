a769d6480c4527e4aa1af6a632aa57eb40e319ad
amitbhalla <amit9815@gmail.com>
2021-08-18 09:58:48 +0530

    Chapter Model added in chapter app

--- chapter/admin.py
+++ chapter/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+
+from chapter.models import Chapter
+
+
 # Register your models here.
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ['course', 'chapter_type', 'index', 'child_chapters']
+
+
+admin.site.register(Chapter, ChapterAdmin)
--- chapter/models.py
+++ chapter/models.py
@@ -1,3 +1,23 @@
+import uuid
+
+
 from django.db import models
 
-# Create your models here.
+
+from course.models import Course
+
+
+chapter_choices = (
+    ('T', 'Text'),
+    ('H', 'Heading'),
+    ('V', 'Videos'),
+    ('L', 'Link'),
+)
+
+
+class Chapter(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='courses')
+    chapter_type = models.CharField(choices=chapter_choices, max_length=150)
+    index = models.IntegerField(null=False)
+    parent_chapter = models.ForeignKey('Chapter', on_delete=models.CASCADE, related_name='child_chapters')
