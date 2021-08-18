2ebb11d7e1d9fae6230d3f22e5251409c0a0977d
amitbhalla <amit9815@gmail.com>
2021-08-18 10:17:17 +0530

    LinkChapter Model added in chapter app

--- chapter/models.py
+++ chapter/models.py
@@ -15,9 +15,18 @@ chapter_choices = (
 )
 
 
+# Holds the chapter object
 class Chapter(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='courses')
     chapter_type = models.CharField(choices=chapter_choices, max_length=150)
     index = models.IntegerField(null=False)
     parent_chapter = models.ForeignKey('Chapter', on_delete=models.CASCADE, related_name='child_chapters')
+
+
+# Object inside a chapter
+class LinkChapter(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    chapter = models.OneToOneField(Chapter, on_delete=models.CASCADE, related_name='link_chapter')
+    title = models.CharField(max_length=150)
+    url = models.URLField(max_length=150)
