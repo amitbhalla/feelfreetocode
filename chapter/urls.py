7ddc0fc2d6f38cfdd81f41838ae87a51d628cf71
amitbhalla <amit9815@gmail.com>
2021-08-19 16:53:35 +0530

    Added Chapter Create View

--- chapter/urls.py
+++ chapter/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import chapter_types_view, video_platform_view, ChapterListView
+from .views import chapter_types_view, video_platform_view, ChapterListCreateView
 
 
 # api/chapters/
 urlpatterns = [
     path("chapter-types/", chapter_types_view, name="chapter-type-view"),
     path("video-platforms/", video_platform_view, name="video-platform-listview"),
-    path("", ChapterListView.as_view(), name="chapter-list"),
+    path("", ChapterListCreateView.as_view(), name="chapter-listcreateview"),
 ]
--- chapter/views.py
+++ chapter/views.py
@@ -1,38 +1,21 @@
 from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView
 from rest_framework import status
 
-from chapter.models import (
-    Chapter,
-    TextChapter,
-    HeadingChapter,
-    VideoChapter,
-    LinkChapter,
-    chapter_choices,
-    chapter_choises_description,
-    video_platform_choises,
-)
-from chapter.serializers import (
-    ChapterSerializer,
-    TextChapterSerializer,
-    HeadingChapterSerializer,
-    VideoChapterSerializer,
-    LinkChapterSerializer,
-)
+
+from chapter.models import Chapter, TextChapter, HeadingChapter, VideoChapter, LinkChapter, chapter_choices, chapter_choises_description, video_platform_choises
+from chapter.serializers import ChapterSerializer, TextChapterSerializer, HeadingChapterSerializer, VideoChapterSerializer, LinkChapterSerializer
+from core.permissions import IsAdminUserOrReadOnly
 
 
 @api_view(["GET"])
 def chapter_types_view(request):
     def SearchDescription(id):
-        for (
-            _id,
-            description,
-        ) in chapter_choises_description:  # Cannot use id twice hence _id
+        for (_id, description,) in chapter_choises_description:  # Cannot use id twice hence _id
             if id == _id:
                 return description
-
     types = map(
         lambda e: dict(id=e[0], type=e[1], description=SearchDescription(e[0])),
         chapter_choices,
@@ -46,7 +29,8 @@ def video_platform_view(request):
     return Response(platform)
 
 
-class ChapterListView(ListAPIView):
+class ChapterListCreateView(ListCreateAPIView):
+    permission_classes = [IsAdminUserOrReadOnly]
     queryset = Chapter.objects.all()
     serializer_class = ChapterSerializer
-    ordering = ["index"]
+
